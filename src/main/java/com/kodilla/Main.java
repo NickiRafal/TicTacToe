b5d65869fc5181762cbaad81b10a9ecdbee4bc03
RafalNicki <Nicol79@op.pl>
2023-07-05 13:08:43 +0200

    [9.2] TicTacToe update4 java17

--- src/main/java/com/kodilla/Main.java
+++ src/main/java/com/kodilla/Main.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        //Pobieranie danych graczy
         User[] players = DataUser.playersData();
+        //ustawianie planszy
         char[][] board = DataUser.boardDimensions();
+        // informacja o graczach
         System.out.println(players[0].getUsername()+" będzie grał figurą "+ players[0].getX());
         System.out.println(players[1].getUsername()+" będzie grał figurą "+ players[1].getX());
+        //ustawianie figury na wybranym polu
         PlayGame.checkBox(board,players[0]);
 
     }
--- src/main/java/com/kodilla/PlayGame.java
+++ src/main/java/com/kodilla/PlayGame.java
@@ -6,14 +6,16 @@ public class PlayGame {
 
 
     public static int printBoards(char[][] board){
-
+        //Funkcja Drukowanie planszy
         int dimension = board.length;
         System.out.print("\t");
         for (int i = 0;i <dimension;i++){
+            //drukowanie indeksów nagłówka planszy
             System.out.print(i+"\t");
         }
         System.out.println();
         for (int row = 0; row< board.length;row++){
+            //Drukowanie indeksów wierszy planszy
             System.out.print(row+" |\t");
             for (int column = 0;column < board[row].length;column++){
                 System.out.print(board[row][column]+"\t");
@@ -24,12 +26,13 @@ public class PlayGame {
         return dimension;
     }
     public static void checkBox(char[][] board, User user) {
+        //Pobieranie indeksów do ustawienia figury na planszy
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj index wiersza");
         int rows = scanner.nextInt();
         System.out.println("Podaj index kolumny");
         int column = scanner.nextInt();
-        scanner.nextLine();
+        scanner.close();
 
         if (rows >= 0 && rows < board.length && column >= 0 && column < board[0].length) {
             board[rows][column] = user.getX(); // Ustawienie figury gracza w podanym indeksie
